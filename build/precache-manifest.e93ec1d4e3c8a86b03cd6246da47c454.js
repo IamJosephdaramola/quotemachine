14e9a4a2a014745794b87826d0ff88ff3662b5b3
Joseph Daramola <daramolaj@gmail.com>
2020-02-29 08:15:49 +0100

    ready for production

---
+++ build/precache-manifest.e93ec1d4e3c8a86b03cd6246da47c454.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58073b9d00d3221d5d82a1b4ce87f90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50e6b18a3ef3a0bfcd18",
+    "url": "/static/css/main.666eb930.chunk.css"
+  },
+  {
+    "revision": "55b60e2999b3c7eb7888",
+    "url": "/static/js/2.c74b8238.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.c74b8238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e6b18a3ef3a0bfcd18",
+    "url": "/static/js/main.4a9069ba.chunk.js"
+  },
+  {
+    "revision": "3df709c33d626033b05b",
+    "url": "/static/js/runtime-main.62113528.js"
+  },
+  {
+    "revision": "f7a34dc9d7bbfbd8de40bb99aa171cbd",
+    "url": "/static/media/img1.f7a34dc9.jpg"
+  }
+]);
